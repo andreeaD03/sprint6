292222cd7c4c0b71581ce8e6c85fe4467a55376f
andreea dan <andreeadan0302@gmail.com>
2024-06-29 23:01:48 +0300

    informatii js intro

--- ex0/index.js
+++ ex0/index.js
@@ -1,1 +1,65 @@
-console.log("hello world!") 
+// Tipuri de date:
+// Date primitive
+// 1. Nedefinit ( undefined) : undefned
+// 2. N4la (object): null
+// 3. Boolean(boolan) : true / false
+// 4. Numere (number)
+//      -negative
+//      -reale 123.5
+//      -stiinifice: 1.3e+4 => 1300
+//      -stiintiifice: 1.2e-3 => 0.0012
+//      -Bigint (bigint) se creeaza sub forma de Bigint
+//      -Infinity, -infinity
+//      - NaN (not a number)
+// 5. String (string) : "", "1" , "aab" , "ana are mere" , "string cu / rand nou"
+// 6. Obiect (object): {} , {"cheie": "valoare"}
+// 7. Liste (object): [] , [ 123, ana, true]
+//
+// Date obiectuale *
+// 8. Obecte instantiabile (object): date, oric tip costumizabil, bigInt
+//
+// TIPUL UNEI VALORI SE AFLA CU COMANDA "typeof"
+//
+// VARIABILE:
+// 1. const => mu se modifica
+// 2. let => se pot modifica
+// 3. var => deprecated => nu se mai folosste.
+
+
+// Opertori aritmetici
+//  *  inmultire
+//  /  impartire
+//  +  adunare
+//  -  scadere
+//  %  modulo (rstul imparirii - catul)
+// Operatori binari
+//   &, |, ^,
+
+//  000 = 1
+//  001 = 2
+//  010 = 3
+//  011 = 4
+//  100 = 5
+//  101 = 6
+//  110 = 7
+//  111 = 8
+
+const alfa = "2"
+const beta = 2
+
+const n = (alfa === beta)
+console.log(n)
+
+
+// Operatii logice (boolene) => da rezulat un tip boolean (true / false)
+// > , < , => , <=
+// == ( se veifica doar valoarea, se ignora tipul)
+// === ( se verifica valoarea si tipul)
+// && (and) si
+// || (or) sau
+// ! (not) nrgare, nu
+
+
+
+
+
