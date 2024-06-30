69296eb0054787656813b899a4a4b401666d94bc
andreea dan <andreeadan0302@gmail.com>
2024-06-30 19:46:37 +0300

    ex1

---
+++ ex1/index.js
@@ -0,0 +1,19 @@
+const number = 50;
+const impar =  ( "nu este par")
+if (number % 10 === 0) {
+    console.log (number);
+
+} else{
+    console.log(impar)
+}
+
+
+let numar = 121;
+const par = ("true")
+const impaR = ("false")
+
+if (numar % 10 === 0) {
+    console.log (par)
+} else {
+    console.log (impaR)
+}
