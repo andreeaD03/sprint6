94ac5e376dde1149724f5e6312ab2c46d7e39b7f
andreea dan <andreeadan0302@gmail.com>
2024-07-03 13:14:26 +0300

    tema 2 sprint 6

---
+++ ex2/index.js
@@ -0,0 +1,11 @@
+
+
+
+function tellFortune(nrCopii = 2, partener = Bianca, locatia = "Timisoara", cariera = "Inginer") {
+
+    const future = 'Andrei este un {cariera} locuieste in { locatia } "casatorit cu" ${ partener } "si au"  ${ nrCopii } copii'
+    console.log(future)
+
+
+}
+
