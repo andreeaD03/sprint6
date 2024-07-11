b351b02ef929dfdff64efa194f98906d5d750f88
andreea dan <andreeadan0302@gmail.com>
2024-07-11 21:57:16 +0300

    tema 3 s6 finlizata

---
+++ ex3/index.js
@@ -0,0 +1,26 @@
+function getDay(days){
+    switch(days){
+        case 0:
+            day = "luni";
+            break;
+          case 1:
+            day = "marti";
+            break;
+          case 2:
+            day = "miercuri";
+            break;
+          case 3:
+            day = "joi";
+            break;
+          case 4:
+            day = "vineri";
+            break;
+          case 5:
+            day = "sambata";
+            break;
+          case  6:
+            day = "duminica";
+    }
+}
+
+console.log(getDay(1));
